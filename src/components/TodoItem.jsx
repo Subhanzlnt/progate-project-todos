55d8da986e5b9cbf8a9891d2d7ce00661e37314f
Subhanzlnt <aan.ananta81@gmail.com>
2024-06-10 15:00:31 +0700

    update

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const TodoItem = ({ todo, handleStatus }) => {
+const TodoItem = ({ todo, handleStatus, handleDelete }) => {
   const getTodoTitleStyle = () => {
     return {
       textDecoration: todo.done ? 'line-through' : 'none'
@@ -10,11 +10,18 @@ const TodoItem = ({ todo, handleStatus }) => {
   return (
     <div className="flex items-center justify-between p-2 border-b">
       <p style={getTodoTitleStyle()}>{todo.text}</p>
-      <button 
-        className="btn btn-primary btn-sm" 
-        onClick={() => handleStatus(todo.id)}>
-        {todo.done ? 'Undo' : 'Complete'}
-      </button>
+      <div>
+        <button 
+          className="btn btn-primary btn-sm mr-2" 
+          onClick={() => handleStatus(todo.id)}>
+          {todo.done ? 'Undo' : 'Complete'}
+        </button>
+        <button 
+          className="btn btn-danger btn-sm" 
+          onClick={() => handleDelete(todo.id)}>
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
